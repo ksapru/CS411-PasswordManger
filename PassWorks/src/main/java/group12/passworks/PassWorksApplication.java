b15c1feea21157791001f2118ade0776124c6b32
Noahjb4 <noahreed1@gmail.com>
2022-04-07 23:33:31 -0400

    Inital Web App upload

---
+++ PassWorks/src/main/java/group12/passworks/PassWorksApplication.java
@@ -0,0 +1,13 @@
+package group12.passworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassWorksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassWorksApplication.class, args);
+    }
+
+}
---
+++ PassWorks/src/test/java/group12/passworks/PassWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package group12.passworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassWorksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
